c47b7a416d77e7518ade29e036eeb4d289281423
Unknown <krys.nagorny@gmail.com>
2018-01-07 16:20:59 +0100

    + add https to swagger + modify link

--- src/database/config.js
+++ src/database/config.js
@@ -1,4 +1,4 @@
 //link to db
-const url = 'mongodb://krystian_mongo:eventhub123@ds135817.mlab.com:35817/heroku_5tljt8fj';
+const url = 'mongodb://krystian_mongo:eventhub123@ds247077.mlab.com:47077/heroku_9ns754rl';
 
 export  default url;
